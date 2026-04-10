5088472c1b4872a2d30c475d30c2fc0e86d9ca26
Egor Starikov <egor.starikov2007@gmail.com>
2026-04-10 19:07:50 +0300

    реализован printer

--- src/main/kotlin/stack/SingleLinkedStack.kt
+++ src/main/kotlin/stack/SingleLinkedStack.kt
@@ -6,7 +6,7 @@ import org.example.list.SingleLinkedList
 class SingleLinkedStack : Stack, SingleLinkedList() {
 
     override val isEmpty: Boolean
-        get() = (size == 0)
+        get() = size == 0
 
     override fun push(value: Int) {
         addFirst(value)
@@ -16,6 +16,7 @@ class SingleLinkedStack : Stack, SingleLinkedList() {
         if (isEmpty) throw NoSuchElementException()
         return get(0)
     }
+
     override fun pop(): Int {
         if (isEmpty) throw NoSuchElementException()
         val result = get(0)
